32648aa2ffd7a71c6f753d37ad53952c3d045e88
nikhil <nikdongre98@gmail.com>
2023-07-23 21:51:54 +0530

    Added remove cartitem after payment fron the user cartitem

--- orders/admin.py
+++ orders/admin.py
@@ -2,8 +2,19 @@ from django.contrib import admin
 from .models import Payment,Order,OrderProduct
 
 # Register your models here.
+class OrderProductInline(admin.TabularInline):
+    model = OrderProduct
+    readonly_fields =('payment','user','product','quantity','product_price','ordered')
+    extra = 0
 
 
-admin.site.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display=['order_number', 'full_name', 'email', 'phone', 'city','order_total','tax','status','ip','is_ordered']
+    list_filter = ['status', 'is_ordered']
+    search_fields = ['order_number', 'first_name','last_name' 'email', 'phone']
+    list_per_page = 20
+    inlines = [OrderProductInline]
+
+admin.site.register(Order,OrderAdmin)
 admin.site.register(Payment)
 admin.site.register(OrderProduct)
--- orders/views.py
+++ orders/views.py
@@ -2,8 +2,11 @@ import datetime
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from carts.models import Cart,CartItem
+from store.models import Product
 from .models import Order
 from .forms import Orderform
+import json
+from .models import Payment,OrderProduct
 # Create your views here.
 
 def place_order(request,total=0,quantity=0):
@@ -74,6 +77,62 @@ def place_order(request,total=0,quantity=0):
     
 
 def payments(request):
+   body = json.loads(request.body)
+   order = Order.objects.get(user = request.user, order_number = body['orderID'],is_ordered = False)
+
+
+    
+   print(body)
+   #    store transaction details in payment model
+   payment = Payment(
+       user = request.user,
+       payment_id = body['transID'],
+       payment_method = body['payment_method'],
+       amount_paid = order.order_total,
+       status = body['status'],
+
+
+   )
+   payment.save()
+   order.payment = payment
+   order.is_ordered =True
+   order.save()
+
+   #    Move the ordered product to ordered table 
+   cart_item = CartItem.objects.filter(user = request.user)
+   for item in cart_item:
+
+    orderproduct = OrderProduct()
+    orderproduct.order = order
+    orderproduct.payment = payment
+    orderproduct.user = request.user
+    orderproduct.product = item.product
+    orderproduct.quantity = item.quantity
+    orderproduct.product_price = item.product.price
+    orderproduct.ordered = True
+    orderproduct.save()
+
+    cart_item = CartItem.objects.get(id=item.id)
+    product_variation = cart_item.variations.all()
+    orderproduct = OrderProduct.objects.get(id = orderproduct.id)
+    orderproduct.variations.set(product_variation)
+    orderproduct.save()
+    #    Reduce the quantily of sold 
+
+    product = Product.objects.get(id = item.product.id)
+    product.stock -= item.quantity
+    product.save()
+# clear the cart
+   CartItem.objects.filter(user=request.user).delete()
+
+
+# send order email to customer
+# send transaction and and order number to back to senddata method via json response
+
+
+
+
+
    return render(request, 'orders/payments.html') 
     
 
